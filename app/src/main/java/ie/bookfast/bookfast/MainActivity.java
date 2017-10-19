3d1157f3dd5b3f1b8445929288d84f2d72efc84c
n7d1b <n7d1b@ugrad.cs.ubc.ca>
2017-10-19 12:59:53 +0100

    Basic UI and setting up Map Activity

--- app/src/main/java/ie/bookfast/bookfast/MainActivity.java
+++ app/src/main/java/ie/bookfast/bookfast/MainActivity.java
@@ -1,13 +1,33 @@
 package ie.bookfast.bookfast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button toMapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toMapBtn = (Button) findViewById(R.id.main_to_map_btn);
+        toMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Finding current location...", Toast.LENGTH_SHORT).show();
+                toMap();
+            }
+        });
+    }
+
+    private void toMap(){
+        Intent mapIntent = new Intent(this, MapActivity.class);
+        startActivity(mapIntent);
     }
 }
