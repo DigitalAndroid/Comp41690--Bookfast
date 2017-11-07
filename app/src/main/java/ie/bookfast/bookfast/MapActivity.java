4648be20f703959dfae12f98454a959254411dae
n7d1b <n7d1b@ugrad.cs.ubc.ca>
2017-11-07 20:57:19 +0000

    Trying to get navigation functionality when marker is clicked

--- app/src/main/java/ie/bookfast/bookfast/MapActivity.java
+++ app/src/main/java/ie/bookfast/bookfast/MapActivity.java
@@ -11,10 +11,15 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.kml.KmlDocument;
 import org.osmdroid.bonuspack.location.OverpassAPIProvider;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.bonuspack.routing.RoadNode;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.BoundingBox;
@@ -22,9 +27,13 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
 import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 import static ie.bookfast.bookfast.R.id.map;
 
@@ -65,7 +74,6 @@ public class MapActivity extends AppCompatActivity{
         startPoint = new GeoPoint(latitude, longitude);
         mapController.setCenter(startPoint);
 
-
         //adding in 'my location'
         this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(thisCtx), bookMap);
         this.mLocationOverlay.enableMyLocation();
@@ -78,8 +86,109 @@ public class MapActivity extends AppCompatActivity{
 
         new overpassKML().execute("amenity=library");
 
+        navigateToMarkers();
+
         //filling tile on screen with map
         bookMap.setTileSource(TileSourceFactory.MAPNIK);
+
+    }
+
+    //method to set onclicks for markers - when you click one, you navigate to it.
+    private void navigateToMarkers(){
+        //Lists needed to eventually get geopoint from overlays
+//        List<Overlay> overlayList = bookMap.getOverlays();
+//        List<Marker> markerList = new ArrayList<>();
+
+        for(int i=0; i<bookMap.getOverlays().size(); i++){
+            if(bookMap.getOverlays().get(i) instanceof Marker){
+                ((Marker) bookMap.getOverlays().get(i)).setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker, MapView mapView) {
+                        DrawRoad drawRoad = new DrawRoad(getCurrentLocation(), marker.getPosition());
+                        drawRoad.execute();
+
+                        Toast.makeText(MapActivity.this, "Marker clicked!", Toast.LENGTH_SHORT).show();
+
+                        return true;
+                    }
+                });
+            }
+        }
+
+
+//        //getting all instances of markers from overlays
+//        for(int i=0; i<overlayList.size(); i++){
+//            if(overlayList.get(i) instanceof Marker){
+//
+//                markerList.add((Marker) overlayList.get(i));
+//            }
+//        }
+//
+//        //setting onclick listener for each marker so that you will get navigation to marker if you click it.
+//        for(int i=0; i<markerList.size(); i++){
+//            markerList.get(i).setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+//                @Override
+//                public boolean onMarkerClick(Marker marker, MapView mapView) {
+//                    DrawRoad drawRoad = new DrawRoad(getCurrentLocation(), marker.getPosition());
+//                    drawRoad.execute();
+//
+//                    Toast.makeText(MapActivity.this, "Marker clicked!", Toast.LENGTH_SHORT).show();
+//
+//                    return true;
+//                }
+//            });
+//
+//        }
+
+
+    }
+
+    //we do this in a different thread as it takes some amount of processing power to get route to ucd.
+    private class DrawRoad extends AsyncTask<Void, Void, Road> {
+        GeoPoint startPoint;
+        GeoPoint endPoint;
+
+        public DrawRoad(GeoPoint startPoint, GeoPoint endPoint){
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+        }
+
+        @Override
+        protected Road doInBackground(Void... voids) {
+            RoadManager roadManager = new OSRMRoadManager(MapActivity.this);
+
+            ArrayList<GeoPoint> waypoints = new ArrayList<>();
+            waypoints.add(startPoint);
+            waypoints.add(endPoint);
+
+            Road road = roadManager.getRoad(waypoints);
+
+            return road;
+        }
+
+        protected void onPostExecute(Road road){
+            updateUIWithRoad(road);
+        }
+    }
+
+    public void updateUIWithRoad(Road road){
+        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+
+        bookMap.getOverlays().add(roadOverlay);
+
+        bookMap.invalidate();
+
+        //adding bubble at every new road/turn
+        for (int i=0; i<road.mNodes.size(); i++){
+            RoadNode node = road.mNodes.get(i);
+            Marker nodeMarker = new Marker(bookMap);
+            nodeMarker.setSnippet(node.mInstructions);
+            nodeMarker.setSubDescription(Road.getLengthDurationText(this, node.mLength, node.mDuration));
+            nodeMarker.setPosition(node.mLocation);
+//            nodeMarker.setIcon();
+            nodeMarker.setTitle("Step "+i);
+            bookMap.getOverlays().add(nodeMarker);
+        }
 
     }
 
@@ -95,6 +204,7 @@ public class MapActivity extends AppCompatActivity{
         @Override
         protected FolderOverlay doInBackground(String... tag) {
             OverpassAPIProvider overpassProvider = new OverpassAPIProvider();
+
             BoundingBox oBB = new BoundingBox(startPoint.getLatitude() + 0.25, startPoint.getLongitude() + 0.25,
                     startPoint.getLatitude() - 0.25, startPoint.getLongitude() - 0.25);
             String url = overpassProvider.urlForTagSearchKml(tag[0], oBB, 500, 30);
@@ -108,7 +218,6 @@ public class MapActivity extends AppCompatActivity{
         @Override
         protected void onPostExecute(FolderOverlay kmlOverlay) {
             bookMap.getOverlays().add(kmlOverlay);
-
         }
     }
 
@@ -121,7 +230,9 @@ public class MapActivity extends AppCompatActivity{
         return displaymetrics.widthPixels;
     }
 
-    private void getCurrentLocation(){
+    private GeoPoint getCurrentLocation(){
+        GeoPoint currentGeoPointLocation;
+
         //check permissions to access GPS
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             MainActivity.requestPermission(this);
@@ -145,6 +256,7 @@ public class MapActivity extends AppCompatActivity{
         latitude = currentLocation.getLatitude();
         longitude = currentLocation.getLongitude();
 
+        return currentGeoPointLocation = new GeoPoint(latitude, longitude);
         //TODO: what if user declines permission
     }
 }
