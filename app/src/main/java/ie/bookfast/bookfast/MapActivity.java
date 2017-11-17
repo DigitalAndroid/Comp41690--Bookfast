bf41227368cb51644eb4aabacfb8e16a6d2a472c
n7d1b <n7d1b@ugrad.cs.ubc.ca>
2017-11-17 13:23:15 +0000

    Managed to get it working! Navigate to marker when clicked!

--- app/src/main/java/ie/bookfast/bookfast/MapActivity.java
+++ app/src/main/java/ie/bookfast/bookfast/MapActivity.java
@@ -11,7 +11,6 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
@@ -98,17 +97,11 @@ public class MapActivity extends AppCompatActivity{
 
     //method to set onclicks for markers - when you click one, you navigate to it.
     private void navigateToMarkers(){
-        //Lists needed to eventually get geopoint from overlays
-//        List<Overlay> overlayList = bookMap.getOverlays();
-//        List<Marker> markerList = new ArrayList<>();
-        Log.e("Debugging:", "navigateToMarkers: got here.");
         for(int i=0; i<bookMap.getOverlays().size(); i++){
-            Log.e("Debugging:", "navigateToMarkers: got here2.");
             if(bookMap.getOverlays().get(i) instanceof Marker){
                 ((Marker) bookMap.getOverlays().get(i)).setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
                     @Override
                     public boolean onMarkerClick(Marker marker, MapView mapView) {
-                        Log.e("Debugging:", "navigateToMarkers: got here3.");
                         DrawRoad drawRoad = new DrawRoad(getCurrentLocation(), marker.getPosition());
                         drawRoad.execute();
 
@@ -119,33 +112,6 @@ public class MapActivity extends AppCompatActivity{
                 });
             }
         }
-
-
-//        //getting all instances of markers from overlays
-//        for(int i=0; i<overlayList.size(); i++){
-//            if(overlayList.get(i) instanceof Marker){
-//
-//                markerList.add((Marker) overlayList.get(i));
-//            }
-//        }
-//
-//        //setting onclick listener for each marker so that you will get navigation to marker if you click it.
-//        for(int i=0; i<markerList.size(); i++){
-//            markerList.get(i).setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
-//                @Override
-//                public boolean onMarkerClick(Marker marker, MapView mapView) {
-//                    DrawRoad drawRoad = new DrawRoad(getCurrentLocation(), marker.getPosition());
-//                    drawRoad.execute();
-//
-//                    Toast.makeText(MapActivity.this, "Marker clicked!", Toast.LENGTH_SHORT).show();
-//
-//                    return true;
-//                }
-//            });
-//
-//        }
-
-
     }
 
     //we do this in a different thread as it takes some amount of processing power to get route to ucd.
@@ -189,12 +155,59 @@ public class MapActivity extends AppCompatActivity{
             Marker nodeMarker = new Marker(bookMap);
             nodeMarker.setSnippet(node.mInstructions);
             nodeMarker.setSubDescription(Road.getLengthDurationText(this, node.mLength, node.mDuration));
+            nodeMarker.setIcon(getResources().getDrawable(R.mipmap.direction_marker));
             nodeMarker.setPosition(node.mLocation);
-//            nodeMarker.setIcon();
             nodeMarker.setTitle("Step "+i);
+            determineDirectonImage(node, nodeMarker);
             bookMap.getOverlays().add(nodeMarker);
         }
 
+    }
+
+    //setting the direction icons
+    private void determineDirectonImage(RoadNode node, Marker nodeMarker){
+        switch (node.mManeuverType){
+            case 1: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_continue));
+                break;
+            case 3: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_slight_left));
+                break;
+            case 4: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_turn_left));
+                break;
+            case 5: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_sharp_left));
+                break;
+            case 6: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_slight_right));
+                break;
+            case 7: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_turn_right));
+                break;
+            case 8: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_sharp_right));
+                break;
+            case 12: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_u_turn));
+                break;
+            case 13: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_u_turn));
+                break;
+            case 14: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_u_turn));
+                break;
+            case 24: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_arrived));
+                break;
+            case 27: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 28: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 29: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 30: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 31: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 32: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 33: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            case 34: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_roundabout));
+                break;
+            default: nodeMarker.setImage(getResources().getDrawable(R.mipmap.ic_continue));
+                break;
+        }
     }
 
     @Override
@@ -224,20 +237,17 @@ public class MapActivity extends AppCompatActivity{
         protected void onPostExecute(FolderOverlay kmlOverlay) {
             List<Overlay> overlayList = kmlOverlay.getItems();
 
-            //Log.e("overlayList: ", String.valueOf(overlayList.size()));
-
             for(int i=0; i<overlayList.size(); i++){
-                Log.e("overlayList: ", overlayList.get(i).toString());
                 if(overlayList.get(i) instanceof Marker){
                     Marker marker = (Marker) overlayList.get(i);
+                    marker.setIcon(getResources().getDrawable(R.mipmap.ic_library));
 
                     bookMap.getOverlays().add(marker);
                 }
 
             }
 
-            //bookMap.getOverlays().add(kmlOverlay);
-
+            //after markers placed, set the onclick listeners for them
             navigateToMarkers();
         }
     }
